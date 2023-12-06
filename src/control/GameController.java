97cd1b09e7a1895e1dc0fca1402ed780f9d8e783
ryanrowe449 <43ryanrowe@gmail.com>
2023-12-06 16:08:54 -0500

    added player class
    new nullpointerexception

--- src/control/GameController.java
+++ src/control/GameController.java
@@ -6,12 +6,12 @@ import java.awt.Color;
 
 public class GameController implements CellClickListener { //providing implementation for CellClickListener
     private GUIView guiView;
-    boolean[][] p1ships = new boolean[10][10]; //player 1's ships
+    Player p1;
+    Player p2;
     boolean[][] p1hits = new boolean[10][10];  //represents hit spaces on player 1's board
-    boolean[][] p2ships = new boolean[10][10];
     boolean[][] p2hits = new boolean[10][10];
     boolean isP1 = true;
-    boolean turnStarted = false;
+    boolean turnActive = false;
 
     public GameController(GUIView guiView) {
         this.guiView = guiView; 
@@ -21,13 +21,13 @@ public class GameController implements CellClickListener { //providing implement
     @Override
     public void onCellClicked(int row, int col, JPanel cellPanel) {
         //only listen for clicks once the game has started
-        if (turnStarted){
+        if (turnActive){
             //panel has been clicked, if it's player 1's turn...
             if (isP1){
                 //if that gridspace hasn't been hit yet...
                 if (!p2hits[row][col]){
                 //if clicked cell is an enemy (hit), turn red. if not, turn grey
-                    if (p2ships[row][col]) cellPanel.setBackground(Color.RED);
+                    if (p2.isOccupied(row, col)) cellPanel.setBackground(Color.RED);
                     else cellPanel.setBackground(Color.GRAY);
                     p2hits[row][col] = true;
                     isP1 = false;
@@ -35,30 +35,24 @@ public class GameController implements CellClickListener { //providing implement
             }
             else {
                 if (!p1hits[row][col]){
-                    if (p1ships[row][col]) cellPanel.setBackground(Color.RED);
+                    if (p1.isOccupied(row, col)) cellPanel.setBackground(Color.RED);
                     else cellPanel.setBackground(Color.GRAY);
                     p1hits[row][col] = true;
                     isP1 = true;
                 }
             }
-            turnStarted = false;
+            turnActive = false;
+            updateView();
             System.out.println("Turn is over");
         }
     }
 
     public void startGame() {
         // Additional setup logic if needed
-        //initializing ships, just for testing
-        for (int i = 0; i < 10; i++){
-            p1ships[i][i] = true;
-        }
-        for (int i = 0; i < 10; i++){
-            if(i < 9) p2ships[i][i+1] = true; //like p1 but one gridspace to the right
-            else p2ships[i][i] = true;
-        }
+        playerTurn();
 
-        // Main game loop
-        while (!isGameOver()) {
+        // Main game loop, commenting out now to run the game through the click event
+        /*while (!isGameOver()) {
             // Perform player actions (e.g., handle turns, get input, update game state)
             playerTurn();
 
@@ -69,24 +63,23 @@ public class GameController implements CellClickListener { //providing implement
         }
 
         // Game over logic (e.g., display winner, final statistics)
-        displayGameOver();
+        displayGameOver();*/
     }
 
     private void playerTurn() {
         // Implement logic for handling a player's turn
         // This might include getting input from the player, updating the game state, etc.
-        turnStarted = true;
-        //System.out.println("playerTurn() ran");
+        turnActive = true;
+        System.out.println("playerTurn() ran");
     }
 
     private void updateView() {
         // Update the view to reflect the current game state
         // This involves calling methods on the GameView interface
-        if (!turnStarted){
         guiView.displayMessage("It's the next player's turn."); // Example message
         System.out.println("turn switched");
-        }
         // You might also call other methods on gameView to update the board state, etc.
+        playerTurn();
     }
 
     private boolean isGameOver() {
@@ -99,5 +92,20 @@ public class GameController implements CellClickListener { //providing implement
         // Implement logic to display the game-over state
         // This might include showing the winner, final statistics, etc.
         guiView.displayMessage("Game over! Player X wins!"); // Example message
+    }
+}
+
+class Player{
+    boolean[][] ships;
+    public Player(){
+        ships = new boolean[10][10];
+        //initializing ships, just for testing
+        for (int i = 0; i < 10; i++){
+            ships[i][i] = true;
+        }
+    }
+    public boolean isOccupied(int x, int y){
+        if (ships[x][y]) return true;
+        return false;
     }
 }
